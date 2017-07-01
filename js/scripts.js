0341c5c81c7b51c1372c971e17c07c22576a7050
Arman <turebekov.arman@gmail.com>
2017-07-01 21:24:45 +0600

    Added excluded words

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,35 @@
 function analyzeText(text) {
     var allWords = text.split(/\s+/);
-    t = allWords;
-    fillTable(allWords);
+    var str = "a,in,the,but,on,to,into,onto";
+    var excludedWords = str.split(',');
+    res = [];
+    for (var i = 0; i < allWords.length; i++) {
+        if ($.inArray(allWords[i], excludedWords) == -1) {
+            //alert(allWords[i]);
+            k = $.inArray(allWords[i], res); 
+            if (k == -1) {
+                var obj = {};
+                obj.Name = allWords[i];
+                obj.Translation = allWords[i];
+                obj.Frequency = 1;
+                res.push(obj);
+            } else {
+                res[k].Frequency = res[k].Frequency + 1;
+            }
+        }
+    }
+
+    fillTable(res);
 }
 
 function fillTable(arr) {
     var html = '';
     for (var i = 0; i < arr.length; i++)
     {
-        html += '<tr><td>' + arr[i] +
-            '</td><td>' + 'frequency' + '</td><td>'+'translation'+'</td></tr>';
+        html += '<tr><td>' + arr[i].Name +
+            '</td><td>' + arr[i].Frequency + '</td><td>' + arr[i].Translation + '</td></tr>';
     }
-    $('#results tr').first().after(html);
+    $("#results tbody").append(html);
     setFormVisibility(false);
 }
 
@@ -20,9 +38,9 @@ function setFormVisibility(isVisible) {
         $('#textBlock').show();
         $('#lnkShowForm').css('display', 'none');
         $('#results').css('display', 'none');
+        $("#results tbody").empty();
     } else {
         $('#textBlock').hide();
-        $("#results tbody").empty();
         $('#lnkShowForm').css('display', 'block');
         $('#results').css('display', 'block');
     }
