baeb4070a6e25cc383cfeb99a1623f499615b1dd
ITHelper <ITHelper>
2017-07-21 01:35:55 +0600

    Added pages navigation (menu)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
+﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TextAnalyzerComponent } from './components/text-analyzer/text-analyzer.component';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'analyzer', component: TextAnalyzerComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextAnalyzerComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      FormsModule,
+      HttpModule,
+      RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/text-analyzer/text-analyzer.component.ts
@@ -0,0 +1,57 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'app-text-analyzer',
+  templateUrl: './text-analyzer.component.html',
+  styleUrls: ['./text-analyzer.component.css']
+})
+export class TextAnalyzerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  title = 'Text Analyzer';
+  text = new Observable();
+  newWords = [];
+  excludedWords = [];
+
+  myFunction = function () {
+      console.log('myFunction()');
+      this.text.setValue("some text 2");
+      let arr = this.getNewWords(this.text);
+
+  }
+
+  getNewWords = function () {
+      var allWords = this.text.split(/\s+/);
+      var res = [];
+      this.excludedWords = this.getExcludedWords();
+      for (let word of allWords) {
+          console.log(word);
+      }
+
+      return res;
+  }
+
+  getExcludedWords = function () {
+      var excludedWordsStr = "a,in,the,but,on,to,into,onto,and,of,with,when,has,was,is,were," +
+          "this,its,it,by,will,as,for,have,an,some,many,that,out,can,at,from,"
+          + "not,if,which,"
+          + "one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve";
+      return excludedWordsStr.split(',');
+  }
+
+  onBtnAnalyzeClick() {
+      console.log('onBtnAnalyzeClick();');
+  }
+}
+
+export class Word {
+    id: number;
+    name: string;
+    frequency: number;
+    translation: string;
+}
